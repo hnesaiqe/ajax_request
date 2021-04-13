5716dc7228a7ba881de1df250f0781657bf1ab7c
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-04-13 17:02:38 +1100

    first commit

---
+++ exo/tp_11/script.js
@@ -0,0 +1,8 @@
+   // function displayWeather(data) {
+        //     var name = document.createElement('h1');
+        //     name.innerText = data.name;
+            
+        //     var container = document.getElementById('weather')
+        //     container.innerHTML = '';
+        //     container.appendChild(name);
+        // }
