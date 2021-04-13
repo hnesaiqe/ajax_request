861636bba3cd9e9911a9e902bafc01d6466642a2
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-04-14 10:20:19 +1100

    exercise 3 Giphy

--- exo/tp_11/script.js
+++
@@ -1,8 +0,0 @@
-   // function displayWeather(data) {
-        //     var name = document.createElement('h1');
-        //     name.innerText = data.name;
-            
-        //     var container = document.getElementById('weather')
-        //     container.innerHTML = '';
-        //     container.appendChild(name);
-        // }
